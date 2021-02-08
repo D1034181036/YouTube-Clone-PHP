52965ef8570fa246291d814632479caaaffec09a
Kevin Chen <D1034181036@gm.lhu.edu.tw>
2021-02-08 17:44:36 +0800

    change folder path

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,7 @@ session_start();
 date_default_timezone_set("Asia/Taipei");
 
 try{
-	$con = new PDO("mysql:dbname=VideoTube;host=localhost", "root", "a2453");
+	$con = new PDO("mysql:dbname=VideoTube;host=localhost", "root", "");
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	
 }catch (PDOException $e) {
