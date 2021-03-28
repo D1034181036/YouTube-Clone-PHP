685c1c410441deb29d1d5ca095abbfb5a1353872
Kevin Chen <D1034181036@gm.lhu.edu.tw>
2021-03-28 19:53:37 +0800

    create environment variable

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,15 @@
 <?php
+require_once(dirname(__FILE__) . "/../vendor/autoload.php");
 
 ob_start(); //Turn on output buffering
 session_start();
-
 date_default_timezone_set("Asia/Taipei");
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 try{
-	$con = new PDO("mysql:dbname=VideoTube;host=localhost", "root", "");
+	$con = new PDO("mysql:dbname=$_ENV[DB_NAME];host=$_ENV[DB_HOST]", $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	
 }catch (PDOException $e) {
